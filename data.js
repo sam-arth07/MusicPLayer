424fbe97ce44260fbbaf01ecde34e4cf9a3e6113
sam-arth07 <82860337+sam-arth07@users.noreply.github.com>
2023-05-15 11:12:21 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let songs = [
+    {
+        name : 'Bhaktambar',
+        path : 'assests/Bhaktamar.mp3',
+        artist: 'Yt' 
+    },
+    {
+        name : 'HanumanChalisa',
+        path : 'assests/HanumanChalisa.mp3',
+        artist: 'Yt' 
+    },
+    {
+        name : 'NakodaChalisa',
+        path : 'assests/NakodaChalisa.mp3',
+        artist: 'Yt' 
+    },
+    {
+        name : 'ShantinathMantra',
+        path : 'assests/ShantinathMantra.mp3',
+        artist: 'Yt' 
+    },
+    {
+        name : 'ParshwanathChalisa',
+        path : 'assests/ParshwanathChalisa.mp3',
+        artist: 'Yt' 
+    }
+]
